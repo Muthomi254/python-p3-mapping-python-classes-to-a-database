b427d5b5adf5e32820b6d65afc5e70dad26b3355
muthomi254 <dmunene202020@gmail.com>
2023-12-08 23:50:53 +0300

    completed

--- lib/song.py
+++ lib/song.py
@@ -1,4 +1,45 @@
 from config import CONN, CURSOR
 
+import pytest; 
+import sqlite3
+
+# Establishing a connection to the database
+CONN = sqlite3.connect('music.db')
+CURSOR = CONN.cursor()
+
+# Defining the class their values
 class Song:
-    pass
+    def __init__(self, name, album):
+        self.id = None
+        self.name = name
+        self.album = album
+
+    @classmethod
+    def create_table(cls):
+        sql = """
+            CREATE TABLE IF NOT EXISTS songs (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                album TEXT
+            )
+        """
+
+        CURSOR.execute(sql)
+        CONN.commit()  # To commit changes to the database
+
+    def save(self):
+        sql = """
+            INSERT INTO songs (name, album)
+            VALUES (?, ?)
+        """
+
+        CURSOR.execute(sql, (self.name, self.album))
+        CONN.commit()  # To commit changes to the database
+
+        self.id = CURSOR.lastrowid
+
+    @classmethod
+    def create(cls, name, album):
+        song = Song(name, album)
+        song.save()
+        return song
